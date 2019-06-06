c1df949d348f36974c195a1a0bb34f6a678dfb93
Lesair Valmont <ixra.net@gmail.com>
2019-06-06 15:10:16 -0500

    [AB#59] Material design to not found page.

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,9 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "../withRoot";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  root: {
+    textAlign: "center",
+    paddingTop: theme.spacing.unit * 8,
+    flexGrow: 1
+  }
+});
 
 class NotFound extends React.Component {
   render() {
-    return <p>404 page not found</p>;
+    return (
+      <p>
+        <Typography>Not found page</Typography>
+      </p>
+    );
   }
 }
 
-export default NotFound;
+NotFound.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRoot(withStyles(styles)(NotFound));
