758ac56f684a10bb9ee084e6929217039b45cf54
Lesair Valmont <ixra.net@gmail.com>
2019-06-08 11:33:22 -0500

    [AB#60] Calling Web API.
    To dynamically show usernames.

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 class SignIn extends React.Component {
+  state = {
+    userAccounts: []
+  };
+
+  componentDidMount() {
+    fetch("https://stp-web-api.azurewebsites.net/api/values")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ userAccounts: data });
+      })
+      .catch(console.log);
+  }
+
   render() {
-    return <p>Sign in</p>;
+    return (
+      <>
+        <p>Sign in</p>
+        <p>{this.state.userAccounts.map(ua => `${ua.id}: ${ua.username}`).join(", ")}</p>
+      </>
+    );
   }
 }
 
