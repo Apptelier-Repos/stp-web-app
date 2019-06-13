e76cd6535dd5f5a51bdb8b0aa1c5b2faa65355e1
Lesair Valmont <ixra.net@gmail.com>
2019-06-13 17:10:22 -0500

    [AB#89] Sign in page.
    Basic layout without interactions.

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,4 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import withRoot from "../withRoot";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import signInImg1 from "../images/signIn1.png";
+
+const styles = theme => ({
+  root: {
+    height: "100vh"
+  },
+  image: {
+    backgroundImage: `url(${signInImg1})` /* 1080x1617 */,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+});
 
 class SignIn extends React.Component {
   state = {
@@ -15,13 +59,76 @@ class SignIn extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     return (
-      <>
-        <p>Sign in</p>
-        <p>{this.state.userAccounts.map(ua => `${ua.id}: ${ua.username}`).join(", ")}</p>
-      </>
+      <React.Fragment>
+        <Grid container component="main" className={classes.root}>
+          <CssBaseline />
+          <Grid item xs={false} sm={4} md={7} className={classes.image} />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <div className={classes.paper}>
+              <Typography>
+                {this.state.userAccounts.map(ua => `${ua.id}: ${ua.username}`).join(", ")}
+              </Typography>
+
+              <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Acceder
+              </Typography>
+              <form className={classes.form} noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Nombre de usuario"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Contraseña"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Recuérdame"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Acceder
+                </Button>
+                <Box mt={5}>
+                  <Typography variant="body2" color="textSecondary" align="center">
+                    Powered by Apptelier.
+                  </Typography>
+                </Box>
+              </form>
+            </div>
+          </Grid>
+        </Grid>
+      </React.Fragment>
     );
   }
 }
 
-export default SignIn;
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withRoot(withStyles(styles)(SignIn));
