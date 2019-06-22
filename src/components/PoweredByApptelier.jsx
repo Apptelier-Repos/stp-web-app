05fbd18818e9c373323fc655195f76f5a8924744
Lesair Valmont <ixra.net@gmail.com>
2019-06-22 14:47:05 -0500

    [AB#102] PoweredByApptelier component.
    To be used as footer across the Web application.

---
+++ src/components/PoweredByApptelier.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
+function PoweredByApptelier() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Powered with ❤️ by "}
+      <Link color="inherit" href="https://github.com/apptelier/">
+        Apptelier
+      </Link>
+      .
+    </Typography>
+  );
+}
+
+export default PoweredByApptelier;
